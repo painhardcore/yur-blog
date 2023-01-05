2428124ccce2da99a0612278dd40210ba9374256
Andrey Yurchenkov <painhardcore@gmail.com>
2023-01-05 02:56:33 +0100

    feat(init whole blog): literrally 1st commit

---
+++ src/config.ts
@@ -0,0 +1,135 @@
+import type { SocialObjects } from "./types";
+
+export const SITE = {
+  website: "https://yurchenkov.ru",
+  author: "Andrey Yurchenkov",
+  desc: "Blog about web development and other stuff",
+  title: "Andrey Yurchenkov",
+  ogImage: "astropaper-og.jpg",
+  lightAndDarkMode: true,
+  postPerPage: 3,
+};
+
+export const LOGO_IMAGE = {
+  enable: false,
+  svg: true,
+  width: 216,
+  height: 46,
+};
+
+export const SOCIALS: SocialObjects = [
+  {
+    name: "Github",
+    href: "https://github.com/satnaing/astro-paper",
+    linkTitle: ` ${SITE.title} on Github`,
+    active: true,
+  },
+  {
+    name: "Instagram",
+    href: "",
+    linkTitle: `${SITE.title} on Instagram`,
+    active: false,
+  },
+  {
+    name: "LinkedIn",
+    href: "https://github.com/satnaing/astro-paper",
+    linkTitle: `${SITE.title} on LinkedIn`,
+    active: true,
+  },
+  {
+    name: "Mail",
+    href: "mailto:yourmail@gmail.com",
+    linkTitle: `Send an email to ${SITE.title}`,
+    active: false,
+  },
+  {
+    name: "Twitter",
+    href: "https://twitter.com/painhardcore",
+    linkTitle: `${SITE.title} on Twitter`,
+    active: true,
+  },
+  {
+    name: "Twitch",
+    href: "https://github.com/satnaing/astro-paper",
+    linkTitle: `${SITE.title} on Twitch`,
+    active: false,
+  },
+  {
+    name: "YouTube",
+    href: "https://github.com/satnaing/astro-paper",
+    linkTitle: `${SITE.title} on YouTube`,
+    active: false,
+  },
+  {
+    name: "WhatsApp",
+    href: "https://github.com/satnaing/astro-paper",
+    linkTitle: `${SITE.title} on WhatsApp`,
+    active: false,
+  },
+  {
+    name: "Snapchat",
+    href: "https://github.com/satnaing/astro-paper",
+    linkTitle: `${SITE.title} on Snapchat`,
+    active: false,
+  },
+  {
+    name: "Pinterest",
+    href: "https://github.com/satnaing/astro-paper",
+    linkTitle: `${SITE.title} on Pinterest`,
+    active: false,
+  },
+  {
+    name: "TikTok",
+    href: "https://github.com/satnaing/astro-paper",
+    linkTitle: `${SITE.title} on TikTok`,
+    active: false,
+  },
+  {
+    name: "CodePen",
+    href: "https://github.com/satnaing/astro-paper",
+    linkTitle: `${SITE.title} on CodePen`,
+    active: false,
+  },
+  {
+    name: "Discord",
+    href: "https://github.com/satnaing/astro-paper",
+    linkTitle: `${SITE.title} on Discord`,
+    active: false,
+  },
+  {
+    name: "GitLab",
+    href: "https://github.com/satnaing/astro-paper",
+    linkTitle: `${SITE.title} on GitLab`,
+    active: false,
+  },
+  {
+    name: "Reddit",
+    href: "https://github.com/satnaing/astro-paper",
+    linkTitle: `${SITE.title} on Reddit`,
+    active: false,
+  },
+  {
+    name: "Skype",
+    href: "https://github.com/satnaing/astro-paper",
+    linkTitle: `${SITE.title} on Skype`,
+    active: false,
+  },
+  {
+    name: "Steam",
+    href: "https://github.com/satnaing/astro-paper",
+    linkTitle: `${SITE.title} on Steam`,
+    active: false,
+  },
+  {
+    name: "Telegram",
+    href: "https://t.me/space_tag",
+    linkTitle: `${SITE.title} on Telegram`,
+    active: true,
+  },
+  {
+    name: "Mastodon",
+    href: "https://github.com/satnaing/astro-paper",
+    linkTitle: `${SITE.title} on Mastodon`,
+    active: false,
+  },
+];
---
+++ src/pages/about.md
@@ -0,0 +1,41 @@
+---
+layout: ../layouts/AboutLayout.astro
+title: "About"
+---
+<div>
+  <img src="/assets/harold.webp"  class="sm:w-1/2 mx-auto" alt="coding dev illustration">
+</div>
+
+My name is Andrey Yurchenkov, I live in France and passionate about software developement and memes. So far that's everythingn you should know.
+
+## Social
+Own:
+- [Personal tg blog](https://t.me/space_tag)
+- [Meme tg channel](https://t.me/code500)
+- [Ziglang tg chat](https://t.me/ziglang_en)
+
+Sometimes I write something on my [Medium](https://medium.com/@painhardcore)
+## My Tech Stack
+I'm trying to keep the list of my main tools pretty short:
+- Go
+- PostgreSQL
+- Promotheus
+- Redis
+- Kafka
+
+But if it's not enough for you - this is my extended list what I've used:
+- Python
+- Docker
+- Kubernetes
+- AWS
+- Aerospike
+- Terraform
+- Java
+- Groovy
+- C, C++
+- InfluxDB
+- GRPC
+- Grafana
+- Openshift
+- CSI
+- Robot framework
