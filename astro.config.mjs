1033a625969088b25b59a8f02e8b8e05eaebb270
Andrey Yurchenkov <painhardcore@gmail.com>
2023-04-24 01:52:31 +0200

    fix site address
    Signed-off-by: Andrey Yurchenkov <painhardcore@gmail.com>

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,7 +7,7 @@ import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://astro-paper.pages.dev/",
+  site: "https://yurchenkov.ru/",
   integrations: [
     tailwind({
       config: {
